df9c8838023e83caf19998c6f18b0437c50cb98a
TienThanhBN <tienthanhbn2006@gmail.com>
2024-09-12 14:39:42 +0700

    first commit

---
+++ ung_dung_may_tinh.js
@@ -0,0 +1,8 @@
+function Caculate() {
+let Addition = document.getElementById("Addition").value;
+let Subtraction = document.getElementById("Subtraction").value;
+let Multiplication = document.getElementById("Multiplication").value;
+let Division = document.getElementById("Division").value;
+let Result;
+
+}
